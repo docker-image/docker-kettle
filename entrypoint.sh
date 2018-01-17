529ff06b07796a69b96deccf60bc2ccdf3d538df
tomaer <i@tomaer.com>
2018-01-17 20:20:39 +0800

    完善脚本

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-addgroup -S $KETTLE_GROUP && adduser -h /home/$KETTLE_USER -s /bin/ash -G $KETTLE_GROUP $KETTLE_USER && \
-echo "$KETTLE_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers && \
-chown -R $KETTLE_USER:$KETTLE_GROUP $PDI_HOME/data-integration \
-chown -R $KETTLE_USER:$KETTLE_GROUP $PDI_HOME/datas && \
-chown -R $KETTLE_USER:$KETTLE_GROUP $PDI_HOME/jobs && \
-top
+if [ ! -d "/opt/pdi-ce/account" ]; then
+    addgroup -S $KETTLE_GROUP && adduser -h /home/$KETTLE_USER -s /bin/ash -G $KETTLE_GROUP $KETTLE_USER && \
+        echo "$KETTLE_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers && \
+        chown -R $KETTLE_USER:$KETTLE_GROUP $PDI_HOME/data-integration \
+        chown -R $KETTLE_USER:$KETTLE_GROUP $PDI_HOME/datas && \
+        chown -R $KETTLE_USER:$KETTLE_GROUP $PDI_HOME/jobs && touch /opt/pdi-ce/account
+fi
+echo "Container PDI-CE Runing" && su - hive && tail -lf /opt/pdi-ce/account
 
